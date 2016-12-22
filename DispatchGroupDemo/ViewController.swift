4b8d97778377febcf1bc9f658bdf61eafc241652
Andrew Zahra <andrew@zguy.com>
2016-12-23 06:08:32 +1100

    switch to labels

--- DispatchGroupDemo/ViewController.swift
+++ DispatchGroupDemo/ViewController.swift
@@ -1,15 +1,14 @@
-//
-//  ViewController.swift
-//  DispatchGroupDemo
-//
-//  Created by Andrew Zahra on 11/12/16.
-//  Copyright © 2016 Magic Mobile. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var runButton: UIButton!
+    @IBOutlet var worker1Label: UILabel!
+    @IBOutlet var worker2Label: UILabel!
+    @IBOutlet var worker3Label: UILabel!
+    @IBOutlet var overallLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +20,45 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func runDemo(_ sender: Any) {
+        runButton.isUserInteractionEnabled = false
+        
+        let queue = DispatchQueue(label: "au.com.magicmobile.dispatch", attributes: .concurrent, target: .main)
+        let group = DispatchGroup()
+        
+        overallLabel.text = "Starting workers...\n"
+
+        let labels = [self.worker1Label, self.worker2Label, self.worker3Label]
+        
+        for label in labels {
+            print("*** start worker")
+            queue.async {
+                self.worker(label!)
+            }
+        }
+        
+        group.notify(queue: DispatchQueue.main) { [unowned self] in
+            self.message("All workers done\n", self.overallLabel)
+            self.runButton.isUserInteractionEnabled = true
+        }
+    }
+    
+    func worker(_ label: UILabel) {
+        let duration = arc4random_uniform(5)+5
+        
+        message("sleeping for \(duration) seconds\n", label)
+
+        sleep(duration)
+        
+        message("Finished\n", label)
+    }
+    
+    func message(_ message: String, _ label: UILabel) {
+        print("Update label: \(message)")
+        DispatchQueue.main.async {
+            label.text = message
+            print("done")
+        }
+    }
 }
 
